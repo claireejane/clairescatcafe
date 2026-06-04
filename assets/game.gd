e62a2cec0ad55a4bf1782a1c32fa5ce1d1043777
claire61 <claire@blastworksinc.ca>
2026-06-03 20:57:54 -0700

    1lastthing

--- assets/game.gd
+++ assets/game.gd
@@ -5,7 +5,7 @@ class_name Game extends Node2D
 @onready var outside_spawn: Marker2D = $OutsideSpawn
 @onready var inside_scene: Node2D = $Cafe
 @onready var outside_scene: Node2D = $Outside
-var current_scene: String = "Outside"
+@onready var current_scene: Node2D = $Outside
 @onready var white_cat_bar: TextureProgressBar = $Cafe/UI/WhiteCatBar
 
 # Called when the node enters the scene tree for the first time.
@@ -24,17 +24,17 @@ func _process(delta: float) -> void:
 	pass
 	
 func _on_change_scene() -> void:
-	print("chagning")
-	if current_scene == "Outside":
-		outside_scene.hide()
-		inside_scene.show()
-		inside_scene.update_cats()
-		white_cat_bar.show()
-		current_scene = "Cafe"
-		player.global_position = inside_spawn.global_position
-	else:
-		inside_scene.hide()
-		outside_scene.show()
-		white_cat_bar.hide()
-		current_scene = "Outside"
-		player.global_position = outside_spawn.global_position
+	match current_scene:
+		outside_scene:
+			outside_scene.hide()
+			inside_scene.show()
+			inside_scene.update_cats()
+			white_cat_bar.show()
+			current_scene = inside_scene
+			player.global_position = inside_spawn.global_position
+		inside_scene:
+			inside_scene.hide()
+			outside_scene.show()
+			white_cat_bar.hide()
+			current_scene = inside_scene
+			player.global_position = outside_spawn.global_position
