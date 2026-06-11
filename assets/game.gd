9c806a3f29ed952eb11687a701f552bf1ac9c052
Claire Jane <claireejane05@gmail.com>
2026-06-10 17:20:03 -0700

    adding all sprites for cat except calico top bar and gleeble sprites

--- assets/game.gd
+++ assets/game.gd
@@ -10,12 +10,12 @@ class_name Game extends Node2D
 @onready var cafe_door: CollisionShape2D = $Cafe/Door/DoorIn
 @onready var outside_door: CollisionShape2D = $Outside/Door/DoorIn
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("starting outside")
 	change_to_outside()
-	inside_scene.change_scene.connect(_on_change_scene)
-	outside_scene.change_scene.connect(_on_change_scene)
+	current_scene.change_scene.connect(_on_change_scene)
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,27 +23,25 @@ func _process(delta: float) -> void:
 	pass
 	
 func _on_change_scene() -> void:
-	match current_scene:
-		outside_scene:
+	match current_scene.name:
+		"Outside":
 			change_to_inside()
-		inside_scene:
+		"Cafe":
 			change_to_outside()	
 
 func change_to_outside():
-	inside_scene.hide()
-	cafe_door.set_deferred("disabled", true)
-	outside_door.set_deferred("disabled", false)
-	outside_scene.show()
-	#white_cat_bar.hide()
-	#current_scene = outside_scene
+	current_scene.change_scene.disconnect(_on_change_scene)
+	remove_child(current_scene)
+	current_scene = outside_scene.instantiate()
+	add_child(current_scene)
 	player.global_position = outside_spawn.global_position
+	current_scene.change_scene.connect(_on_change_scene)
 	
 func change_to_inside():
-	outside_scene.hide()
-	outside_door.set_deferred("disabled", true)
-	cafe_door.set_deferred("disabled", false)
-	inside_scene.show()
-	inside_scene.update_cats()
-	#white_cat_bar.show()
-	current_scene = inside_scene
+	current_scene.change_scene.disconnect(_on_change_scene)
+	remove_child(current_scene)
+	current_scene = inside_scene.instantiate()
+	add_child(current_scene)
+	current_scene.update_cats()
 	player.global_position = inside_spawn.global_position
+	current_scene.change_scene.connect(_on_change_scene)
